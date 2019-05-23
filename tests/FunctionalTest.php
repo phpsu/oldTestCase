b6ff4e1d614ecac22de44da9a7550969d2b769ed
Matthias Vogel <m.vogel@andersundsehr.com>
2019-05-23 08:30:13 +0200

    :bug: fix start time test problems

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -1,13 +1,14 @@
 <?php
 declare(strict_types=1);
 
+namespace PhpsuTestcase\FunctionalTest;
+
 use PHPUnit\Framework\TestCase;
 
 class FunctionalTest extends TestCase
 {
     public function testSshProduction()
     {
-        $result = `vendor/bin/phpsu ssh prod "ls -lRh --time-style=long-iso"`;
         $this->assertSame('.:
 total 8.0K
 -rw-r--r-- 1 user user  137 2019-01-11 08:28 index.php
@@ -21,15 +22,38 @@ drwxrwxr-x 2 user user 4.0K 2019-03-09 15:28 storage
 total 36K
 -rw-rw-r-- 1 user user 29K 2019-03-09 15:28 44134427.png
 -rw-rw-r-- 1 user user 306 2019-03-09 15:26 test.log
-', $result);
-        $this->markTestIncomplete('Need better assertions if ssh was successfully.');
+', `vendor/bin/phpsu ssh prod "ls -lRh --time-style=long-iso" --no-ansi`);
     }
 
     public function testSyncProductionLocal()
     {
-        $result = `vendor/bin/phpsu sync prod local`;
+        `rm -rf tmp/localInstance/var/*`;
+        `echo 'DROP DATABASE IF EXISTS sequelmovie;' | mysql -uroot -plocalPw -h local-db`;
+        $this->assertSame('tmp/localInstance:
+var
+
+tmp/localInstance/var:
+', `ls -R tmp/localInstance`);
+        $this->assertSame('Database
+information_schema
+mysql
+performance_schema
+sys
+', `echo 'SHOW DATABASES;' | mysql -uroot -plocalPw -h local-db`);
+        $result = `vendor/bin/phpsu sync prod local --no-ansi`;
         $this->assertStringContainsString('filesystem:Image Uploads: ✔', $result);
         $this->assertStringContainsString('database:app: ✔', $result);
-        $this->markTestIncomplete('Need more assertions if sync was successfully. ls or mysql command ...');
+        $this->assertSame('tmp/localInstance:
+var
+
+tmp/localInstance/var:
+storage
+
+tmp/localInstance/var/storage:
+44134427.png
+test.log
+', `ls -R tmp/localInstance`);
+        $this->assertSame('6d6f917c92b34c7d187ea62d156c1b10
+', `mysqldump -uroot -plocalPw -h local-db sequelmovie --skip-comments | md5sum | cut -f1 -d" "`);
     }
 }
